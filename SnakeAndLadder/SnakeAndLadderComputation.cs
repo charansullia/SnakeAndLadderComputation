2ec1a2cdc61b687841ff92d90a4d86c5f4e61b37
charasullia <charanrajr713@gmail.com>
2021-09-20 22:14:13 +0530

    [charansullia]added:Player reaches exact winning position

--- SnakeAndLadder/SnakeAndLadderComputation.cs
+++ SnakeAndLadder/SnakeAndLadderComputation.cs
@@ -13,33 +13,43 @@ namespace SnakeAndLadder
         public const int NoPlay = 3;
         public static void GameStart()
         {
-            int winningposition = 100;
-            while (playerposition <= winningposition)
+            int exactwinningposition = 100;
+            while (playerposition <= exactwinningposition)
             {
-                Random random = new Random();
-                int diceroll = random.Next(1, 7);
-                int option = random.Next(1, 4);
-                switch (option)
+                while (playerposition != 100)
                 {
-                    case Ladder:
-                        int playerPosition = diceroll + playerposition;
-                        Console.WriteLine("position of the player is:" + playerPosition);
-                        break;
-                    case Snake:
-                        int playerPosition1 = playerposition - diceroll;
-                        if (playerPosition1 < 0)
-                        {
-                            Console.WriteLine("Got Snake:" + playerposition);
-                            //Console.WriteLine("position of the player is:"+playerposition);
-                        }
+                    Random random = new Random();
+                    int diceroll = random.Next(1, 7);
+                    int option = random.Next(1, 4);
+                    switch (option)
+                    {
+                        case Ladder:
+                            int playerPosition = diceroll + playerposition;
+                            if (playerposition > exactwinningposition)
+                            {
+                                Console.WriteLine("player position is:" + playerPosition);
+                            }
+                            //Console.WriteLine("position of the player is:" + playerPosition);
+                            break;
+                        case Snake:
+                            int playerPosition1 = playerposition - diceroll;
+                            if (playerPosition1 < 0)
+                            {
+                                Console.WriteLine("Got Snake:" + playerposition);
+                                //Console.WriteLine("position of the player is:"+playerposition);
+                            }
 
-                        break;
-                    default:
-                        Console.WriteLine("position of the player is:" + playerposition);
-                        break;
+                            break;
+                        default:
+                            Console.WriteLine("position of the player is:" + playerposition);
+                            break;
+                    }
+
                 }
-
+                Console.WriteLine("new position of the player is:" + playerposition);
+                Console.WriteLine("arrived at finish point");
             }
         }
     }
 }
+
