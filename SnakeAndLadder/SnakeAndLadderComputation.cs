a8e0895d736a1f73cbb2dc26457a700157e16774
charasullia <charanrajr713@gmail.com>
2021-09-20 20:53:20 +0530

    [charansullia]added:Player Position Between 1-6

--- SnakeAndLadder/SnakeAndLadderComputation.cs
+++ SnakeAndLadder/SnakeAndLadderComputation.cs
@@ -7,10 +7,12 @@ namespace SnakeAndLadder
     class SnakeAndLadderComputation
     {
         public const int player = 1;
+        public const int playerposition = 0;
         public static void GameStart()
         {
-            int playerposition = 0;
-            Console.WriteLine("position of the player is:"+playerposition);
+            Random random = new Random();
+            int diceroll = random.Next(1, 7);
+            Console.WriteLine("position of the player is:"+diceroll);
         }
     }
 }
